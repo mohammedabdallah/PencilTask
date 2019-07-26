b7d6f6c7e211d62ecbae4e0c39b2ba31de00d607
mohamad.abdallah <mohamad.abdallah@robustastudio.com>
2019-07-26 18:05:51 +0200

    add cors middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,9 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        //to avoid cros origin problem
+        \App\Http\Middleware\Cors::class,
+
     ];
 
     /**
